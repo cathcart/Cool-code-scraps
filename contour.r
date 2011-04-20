4a4b16bf79c0cb581fe470110791db43541ce1cf
Thomas Cathcart <thomas.cathcart+git@gmail.com>
2011-04-20 21:05:59 +0100

    more or less a final version of the scrap

--- contour.r
+++ contour.r
@@ -1,10 +1,13 @@
+#run with R -f final.r
+#load the necessary libraries
+library(lattice)
+
 #import data
 #file="bc_force_surface.dat"
 file="I2_bc_gga.dat"
 data=read.table(file)
 
 #assign names to the data
-
 names(data)=c("x","y","z")
 attach(data)
 
@@ -16,29 +19,21 @@ x2=x[seq(1,length(x),by=stride)]
 y2=y[1:stride]
 #x stride also appears here
 
+#create the z matrix
 z2=matrix(z,nrow=length(x2),ncol=length(y2),byrow=T)
 
+#setup my nonlinear levels
+split_point=-4196.041
+lower_number=10
+upper_number=3
 
+point_delta=(split_point-min(z2))/lower_number
+my_levels=c(seq(min(z2),split_point-point_delta,point_delta),seq(split_point,max(z2),by=(max(z2)-split_point)/upper_number))
+
+#create the contour plot
 pdf("I2_gga_contour.pdf")
 
-split_point=-4196.041
-point_delta=(split_point-min(z2))/10
-
-library(lattice)
-
-my_levels=c(seq(min(z2),split_point-point_delta,point_delta),seq(split_point,max(z2),by=(max(z2)-split_point)/3))
-
-z2
-range(z2)
-my_levels
-
-#filled.contour(x2,y2,z2,col=topo.colors(27),
-#key.title=title(main="Total\nenergy\n(eV)"),
-#xlab=expression(paste("b parameter (",ring(A),")")),
-#ylab=expression(paste("c parameter (",ring(A),")")),
-#plot.axis={axis(1);axis(2);
-#contour(x2,y2,z2,labcex=1.5,nlevels=27,add=T)})
-filled.contour(x2,y2,z2,col=topo.colors(13),
+filled.contour(x2,y2,z2,col=topo.colors(length(my_levels)),
 cex.lab=1.5,
 key.title=title(main="Total\nenergy\n(eV)"),
 xlab=expression(paste("b parameter (",ring(A),")")),
@@ -47,61 +42,19 @@ levels=my_levels,
 plot.axis={axis(1);axis(2);
 contour(x2,y2,z2,labcex=1.5,levels=my_levels,add=T)})
 
-#filled.contour(x2,y2,z2,
-##col=topo.colors(14),
-##col=heat.colors(14),
-##levels=c(seq(0,split_point-point_delta,point_delta)),
-##col=heat.colors(25),
-##xlim = c(4.0,max(x2)),
-##ylim = c(8.0,11.0),
-#key.title=title(main="Total\nenergy\n(meV)"),
-#xlab=expression(paste("b parameter (",ring(A),")")),
-#ylab=expression(paste("c parameter (",ring(A),")")))
-
-
+#create surface plot
 pdf("I2_gga_surface.pdf")
 
-zlim=c(0,max(z2)) # limits in z coordinates 
-theta=30         # see option theta in persp
-phi=30          # see option phi in persp
+seq(1,length(my_levels),by=2)
 
-nrz <- nrow(z2) 
-ncz <- ncol(z2) 
-ncol=14          # the number of colors to use 
-
-couleurs  <- tail(topo.colors(trunc(140 * ncol)),ncol) 
-fcol      <- couleurs[trunc(z2/zlim[2]*(ncol-1))+1] 
-dim(fcol) <- c(nrz,ncz) 
-fcol      <- fcol[-nrz,-ncz]
-
-persp(z2,col=fcol)#,zlim=zlim,theta=theta,phi=phi,zlab="density") 
-
-#require(MASS)
-#kde2dplot<-function(z2,ncol=50,nlevels=20,theta=30,phi=30)
-#my_colours<-tail(topo.colors(1.4*ncol(z2)),ncol(z2))
-#fcol<-my_colours[trunc(z2*(ncol(z2)-1))+1]
-#dim(fcol)<-c(nrow(z2),ncol(z2))
-#fcol
-#fcol<-my_colours[trunc(z2)+1]
-#wireframe(z2,drape=T,col.region=fcol)
-#
-#
-#range(z2)
-#library(grDevices)
-#custom_colours<-colorRampPalette(c("red","orange","blue"),space="rgb",bias=100)
-#wireframe(z2,row.values=x2,column.values=y2,aspect=c(1,0.75),
-#drape=T,colorkey=T,
-#screen = list(z = 30+180, x = -80,y=-10),
-#scales = list( arrows=FALSE, z = list(tick.number=5),x=list(tick.number=5),y=list(tick.number=5)),
-##scales = list(z.ticks=2, arrows=T),
-##col.regions=topo.colors(71),
-#col.regions=custom_colours(71),
-#zlab="Total\nenergy\n(meV)",
-##par.box = list(col=NA,col=NA),
-##col.regions=b[1:71],
-##col.regions=heat.colors(71),
-##col.regions=heat.colors(4),
-##xlim = c(4.0,max(x2)),
-##ylim = c(8.0,max(y2)),
-#xlab=expression(paste("b parameter (",ring(A),")")),
-#ylab=expression(paste("c parameter (",ring(A),")")))
+wireframe(z2,drape=T,
+cex.lab=1.5,
+row.values=x2,column.values=y2,aspect=c(1,0.75),
+at=my_levels,col.regions=topo.colors(length(my_levels)),
+scales = list( arrows=FALSE,cex=1.2, z = list(tick.number=3),x=list(tick.number=3),y=list(tick.number=3)),
+xlab=list(expression(paste("b parameter (",ring(A),")")),cex=1.2),
+ylab=list(expression(paste("c parameter (",ring(A),")")),cex=1.2),
+zlab=list("Total\nenergy\n(meV)",cex=1.4),
+colorkey=list(labels=list(cex.lab=1.5)),
+#colorkey = list(labels=list(at=my_levels[seq(1,length(my_levels),by=2)])),
+screen = list(z = 30+180, x = -80,y=-10))
