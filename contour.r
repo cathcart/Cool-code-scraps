f2b84f65a44cef9ca6e35c221c1911ffb4da75fd
Thomas Cathcart <thomas.cathcart+git@gmail.com>
2011-04-25 17:01:47 +0100

    all done now, this should also have all the split points and strides i used in generating the pictuers too.

--- contour.r
+++ contour.r
@@ -4,7 +4,10 @@ library(lattice)
 
 #import data
 #file="bc_force_surface.dat"
-file="I2_bc_gga.dat"
+file="I2_bc_force.dat"
+#file="I2_bc_gga.dat"
+#file="I2_bc_vdw.dat"
+#file="tmp"
 data=read.table(file)
 
 #assign names to the data
@@ -12,8 +15,8 @@ names(data)=c("x","y","z")
 attach(data)
 
 #reformate the data
-#stride=10
-stride=4
+stride=10#force
+#stride=4#gga,vdw
 x2=x[seq(1,length(x),by=stride)]
 
 y2=y[1:stride]
@@ -23,7 +26,9 @@ y2=y[1:stride]
 z2=matrix(z,nrow=length(x2),ncol=length(y2),byrow=T)
 
 #setup my nonlinear levels
-split_point=-4196.041
+#split_point=-4200#gga
+#split_point=-4250#vdw
+split_point=0.05#force
 lower_number=10
 upper_number=3
 
@@ -31,7 +36,7 @@ point_delta=(split_point-min(z2))/lower_number
 my_levels=c(seq(min(z2),split_point-point_delta,point_delta),seq(split_point,max(z2),by=(max(z2)-split_point)/upper_number))
 
 #create the contour plot
-pdf("I2_gga_contour.pdf")
+pdf("I2_force_contour.pdf")
 
 filled.contour(x2,y2,z2,col=topo.colors(length(my_levels)),
 cex.lab=1.5,
@@ -43,18 +48,18 @@ plot.axis={axis(1);axis(2);
 contour(x2,y2,z2,labcex=1.5,levels=my_levels,add=T)})
 
 #create surface plot
-pdf("I2_gga_surface.pdf")
-
-seq(1,length(my_levels),by=2)
+pdf("I2_force_surface.pdf")
 
 wireframe(z2,drape=T,
-cex.lab=1.5,
+#cex.lab=1.5,
 row.values=x2,column.values=y2,aspect=c(1,0.75),
 at=my_levels,col.regions=topo.colors(length(my_levels)),
-scales = list( arrows=FALSE,cex=1.2, z = list(tick.number=3),x=list(tick.number=3),y=list(tick.number=3)),
-xlab=list(expression(paste("b parameter (",ring(A),")")),cex=1.2),
-ylab=list(expression(paste("c parameter (",ring(A),")")),cex=1.2),
-zlab=list("Total\nenergy\n(meV)",cex=1.4),
-colorkey=list(labels=list(cex.lab=1.5)),
-#colorkey = list(labels=list(at=my_levels[seq(1,length(my_levels),by=2)])),
+scales = list( arrows=FALSE,cex=1.2, z = list(distance=1.2,tick.number=3),x=list(tick.number=3,distance=1.2),y=list(distance=1.2,tick.number=3)),
+xlab=list(expression(paste("b parameter (",ring(A),")")),cex=1.4),
+ylab=list(expression(paste("c parameter (",ring(A),")")),cex=1.4,space="bottom"),
+#zlab=list("Total energy (eV)",cex=1.2,rot=90),#gga,vdw
+zlab=list("Total force",cex=1.2,rot=90),#force
+#title(main="main title", sub="sub-title"),
+#zlab=list("Total\nenergy\n(meV)",cex=1.4),
+colorkey=list(labels=list(main="something",cex.lab=1.5)),
 screen = list(z = 30+180, x = -80,y=-10))
