1abde7521f4d9a989f9da4332e7a0fd5f185f746
Thomas Cathcart <thomas.cathcart+git@gmail.com>
2011-10-25 18:32:45 +0100

    working PSO

--- swarm.py
+++ swarm.py
@@ -1,3 +1,4 @@
+import math
 import random
 class Vector:
   def __class__(self):
@@ -21,92 +22,122 @@ class Vector:
     return Vector([time*self.data[i] for i in range(len(self.data))])
   def __getitem__(self,index):
     return self.data[index]
+  def __setitem__(self,index,value):
+    self.data[index]=value
 
 class Particle:
-
   def __init__(self,data):
     self.position=Vector(data[0])
     self.velocity=Vector(data[1])
-    self.local_extrema=Vector(data[0])
-
-  #def __repr__(self):
-  #  return repr([self.position,self.velocity])
-  
-  def update_position(self,new_position):
-    self.position=self.position+Vector(new_position) 
-
+    self.local_min=Vector(data[0])
+  def update_position(self):
+    self.position=self.position+self.velocity
   def update_velocity(self,new_velocity):
     self.velocity=self.velocity+Vector(new_velocity)
-
-  def update_local_extrema(self):
-    if self.position < self.local_extrema:
-      self.local_extrema=self.position
-
+  def update_local_min(self):
+    if self.position < self.local_min:
+      self.local_min=self.position
   def update(self,data):
     self.update_velocity(data)
     self.update_position()    
-    self.update_local_extrema()
-
-  def position(self):
-    print "dear god you're calling the position function/method"
-    return self.position
-
-  def cost(self,value):
+    self.update_local_min()
+  def set_cost(self,value):
     self.cost=value
 
-  def print_cost(self):
-    return self.cost
+def distance(one,two):
+  return math.sqrt(sum([(one[i]-two[i])**2 for i in range(len(one))]))
+
+def grouping(vector_list,n):
+  l=[]
+  tmp_list=vector_list
+  groups=[]
+  t=len(vector_list)/n
+  for one in vector_list[0::len(vector_list)/n]:
+    sorted_list=sorted(tmp_list,key=lambda x: distance(one.position,x.position))
+    groups.append(sorted_list[0:len(vector_list)/n])
+    tmp_list=tmp_list[len(vector_list)/n::]
+  return groups
 
 def r():
   yield random.random()
 
-def function(parameters):
+def my_function(parameters):
   [x1,x2]=parameters
   return 100*(x2-x1**2)**2+(1-x1)**2
 
-
-if __name__=="__main__":
-  N=5#number of particles
+def run(N,iterations,grouping_N,min_p,max_p,function):
+  #N=20#number of particles
+  #grouping_N=6#number of groups to slip the swarm into for the local group mins
+  #iterations=100#number of iterations to run
+  #min_p=Vector([0,0])#min parameters
+  #max_p=Vector([20,20])#max_parameters
+  K_l=1.0#local vector constant divided by the particle mass
+  K_g=1.0#global vector constant divided by the particle mass
+  K_gv=1.0#group vector constant divided by the particle mass
+  #[K_l,K_g,k_gv]=constants
+  p=len(min_p)#number of parameters
   swarm=[]#list of swarm particles
-  p=2#number of parameters
-  min_p=Vector([0,0])#min parameters
-  max_p=Vector([1,1])#max_parameters
-  K_l=1#local vector constant divided by the particle mass
-  K_g=1#global vector constant divided by the particle mass
-  #initialise position and velocity for each particle
 
+  #initialise position and velocity for each particle
   for particle in range(N):
     position=[(min_p+(max_p-min_p)*next(r()))[i] for i in range(p)]
     velocity=[((max_p-min_p)*(2*next(r())-1))[i] for i in range(p)]
     swarm.append(Particle([position,velocity]))
 
   #distribute and calculate the value of the function at all of these points
-  for iteration in range(10):
+  min_position=min_p
+
+  for iteration in range(iterations):
+
+#    print "%d %f" %(iteration,min_position[0])
     for particle in swarm:
-      particle.cost(function(particle.position))
+      try:
+        particle.set_cost(function(particle.position))
+      except:
+        particle.set_cost(9999)
+
     #update all the local extrema and decide the new global extrema
-    particles_cost=[particle.print_cost() for particle in swarm]
-    min_index=particles_cost.index(min(particles_cost))
-    print "here we are"
-    t=swarm[min_index]
-    print type(swarm)
-    print swarm[0]
-    print min_index
-    print type(min_index)
-    print 
-    print (swarm[min_index]).cost()
+    min_position=sorted(swarm,key=lambda x: x.cost)[0].position
+    groups=grouping(swarm,grouping_N)
+    for group in groups:
+      group_position=sorted(group,key=lambda x: x.cost)[0].position
+      for particle in group:
 
+        global_vector=min_position-particle.position
+        local_vector=particle.local_min-particle.position
+        group_vector=group_position-particle.position
 
-    quit()
-    print type(t)
-    print t.position()
-    print (swarm[min_index]).position()
-    min_position=Vector(swarm[min_index].position())
-    #update paticle velocities, positions and local_extrema
-    for particle in swarm:
-      local_vector=particle.local_extrema()-particle.position()
-      global_vector=min_position-particle.position()
-      [r_l,r_g]=[next(r()) for i in range(2)]
-      update_vector=r_l*local_vector+r_g*global_vector
-      particle.update(update_vector)
-      
+        [r_l,r_g,r_gv]=[next(r()) for i in range(3)]
+
+        update_vector=(K_l*r_l*local_vector+K_g*r_g*global_vector+K_gv*r_gv*group_vector)
+        particle.update(update_vector)
+
+        #check min
+        mi=particle.position-min_p
+        for i in [y for y in mi if y <= 0]:
+          x=mi.data.index(i)
+          particle.position[x]=min_p[x]
+          particle.velocity[x]=0
+
+        #check max
+        ma=particle.position-max_p
+        for i in [y for y in ma if y >= 0]:
+          x=ma.data.index(i)
+          particle.position[x]=max_p[x]
+          particle.velocity[x]=0
+
+  p=sorted(swarm,key=lambda x: x.cost)[0].position
+  return [math.sqrt(sum([i**2 for i in p])),p]
+
+def test(parameters):
+  [N,iterations]=[int(x) for x in parameters]
+  min_p=Vector([0,0])
+  max_p=Vector([2,2])
+  return run(N,iterations,1,min_p,max_p,my_function)[0]
+
+if __name__=="__main__":
+  N=20
+  iterations=100
+  min_p=Vector([0,0])
+  max_p=Vector([2,2])
+  return run(N,iterations,1,min_p,max_p,my_function)[1]
