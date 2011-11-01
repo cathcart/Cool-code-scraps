fea2518b5f7740544b5dd821cbb873a173f69ec2
Thomas Cathcart <thomas.cathcart+git@gmail.com>
2011-11-01 16:30:03 +0000

    PSO now actually works. take parameters from the wiki PSO page linked paper [pedersen]

--- swarm.py
+++ swarm.py
@@ -1,7 +1,6 @@
 import math
 import random
 import multiprocessing
-
 class Vector:
   def __class__(self):
     return "Vector"
@@ -32,10 +31,11 @@ class Particle:
     self.position=Vector(data[0])
     self.velocity=Vector(data[1])
     self.local_min=Vector(data[0])
+    self.mass=0.8
   def update_position(self):
     self.position=self.position+self.velocity
   def update_velocity(self,new_velocity):
-    self.velocity=self.velocity+Vector(new_velocity)
+    self.velocity=self.mass*self.velocity+Vector(new_velocity)
   def update_local_min(self):
     if self.position < self.local_min:
       self.local_min=self.position
@@ -48,6 +48,15 @@ class Particle:
 
 def distance(one,two):
   return math.sqrt(sum([(one[i]-two[i])**2 for i in range(len(one))]))
+
+def bound(x,l,u):
+
+  if x < l :
+    return l
+  if x > u :
+    return u
+
+  return x
 
 def grouping(vector_list,n):
   l=[]
@@ -73,9 +82,10 @@ def run(N,iterations,grouping_N,min_p,max_p,function):
   #iterations=100#number of iterations to run
   #min_p=Vector([0,0])#min parameters
   #max_p=Vector([20,20])#max_parameters
-  K_l=1.0#local vector constant divided by the particle mass
-  K_g=1.0#global vector constant divided by the particle mass
-  K_gv=1.0#group vector constant divided by the particle mass
+  K_l=0.0#local vector constant 
+  K_g=2.8446#global vector constant 
+  K_gv=0.0#group vector constant
+  mass=-0.3328
   #[K_l,K_g,k_gv]=constants
   p=len(min_p)#number of parameters
   swarm=[]#list of swarm particles
@@ -92,14 +102,9 @@ def run(N,iterations,grouping_N,min_p,max_p,function):
   for iteration in range(iterations):
 
 #    print "%d %f" %(iteration,min_position[0])
-    p=multiprocessing.Pool(4)
+    p=multiprocessing.Pool(6)
     cost_results=p.map(function,[particle.position for particle in swarm])
     [swarm[i].set_cost(cost_results[i]) for i in range(len(swarm))]
-#    for particle in swarm:
-#      try:
-#        particle.set_cost(function(particle.position))
-#      except:
-#        particle.set_cost(9999)
 
     #update all the local extrema and decide the new global extrema
     min_position=sorted(swarm,key=lambda x: x.cost)[0].position
@@ -115,7 +120,13 @@ def run(N,iterations,grouping_N,min_p,max_p,function):
         [r_l,r_g,r_gv]=[next(r()) for i in range(3)]
 
         update_vector=(K_l*r_l*local_vector+K_g*r_g*global_vector+K_gv*r_gv*group_vector)
-        particle.update(update_vector)
+        velocity=mass*particle.velocity+update_vector
+        velocity=[bound(velocity[i],-1*(max_p-min_p)[i],(max_p-min_p)[i]) for i in range(len(velocity))]
+        particle.velocity=Vector(velocity)
+        position=particle.position+particle.velocity
+        position=[bound(position[i],min_p[i],max_p[i]) for i in range(len(position))] 
+        particle.position=Vector(position)
+        #particle.update(update_vector)
 
         #check min
         mi=particle.position-min_p
@@ -141,9 +152,9 @@ def test(parameters):
   return run(N,iterations,1,min_p,max_p,my_function)[0]
 
 if __name__=="__main__":
-  N=60
-  iterations=10000
+  #best=[int(x) for x in run(100,10000,1,Vector([5,10]),Vector([100,100]),test)[1]]
+  #print best
   min_p=Vector([0,0])
-  max_p=Vector([2,2])
-  number_of_groups=4
-  print run(N,iterations,number_of_groups,min_p,max_p,my_function)[1]
+  max_p=Vector([20,20])
+  #print run(best[0],best[1],1,min_p,max_p,my_function)
+  print run(50,400,1,min_p,max_p,my_function)
